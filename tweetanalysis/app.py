9b8f43bfbbcc320a4aa622775736c237260e525a
Marian-Bernice Haligah <55466452+Marian-Bernice@users.noreply.github.com>
2022-10-13 08:53:35 +0000

    Update app.py

--- tweetanalysis/app.py
+++ tweetanalysis/app.py
@@ -252,8 +252,8 @@ if authentication_status:
         b1, b2 = st.columns(2)
         # positive   
         b1.write("**Words in POSITIVE sentiment**")
-        bchart1 = px.histogram(top20p, x='Common words', y='Count', hover_data=['#'], height=600, color_discrete_sequence=["#00CCFF"])
-        b1.bar_chart(bchart1, use_container_width=True)
+        bchart1 = px.bar_chart(top20p, x='Common words', y='Count', hover_data=['#'], height=600, color_discrete_sequence=["#00CCFF"])
+        b1.plotly_chart(bchart1, use_container_width=True)
         
         # Download
         mybuff = StringIO()
